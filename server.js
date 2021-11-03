99a8cd84bf9d8a9d5a4286659a3d0d3ee0be09b6
Bhavin <bhavin1625p@gmail.com>
2021-11-03 09:31:08 +0530

    skeleton structure defined for backend and added express and mongoose

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/homes", require("./routes/homes"));
+app.use("/guests", require("./routes/users"));
+// app.use("/hosts", require("./routes/hosts"));
+app.use("/bookings", require("./routes/bookings"));
+
+app.get("/", (req, res) => {
+    res.send(`
+    <h1> Airbnb Clone </h1>
+    `);
+});
+
+// DB Config
+const uri = config.get("mongoURI");
+
+// Connect to Mongodb
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }) // Adding new mongo url parser
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err));
+
+app.listen(port, () => {
+    console.log(`Server Started at http://localhost:${port}`);
+});
