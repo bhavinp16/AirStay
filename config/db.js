803c6435f2dda3207ab882a311cea9f52b7b00a9
Bhavin <bhavin1625p@gmail.com>
2021-11-03 09:37:53 +0530

    minor change in gitignore

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// config a module we included by yarn which gives access to json inside default.json
+const config = require('config');
+const db = config.get('mongoURI');
+
+// As mongoose returns promises
+const connectDB = () => {
+    mongoose
+        .connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        })
+        .then(() => console.log('MongoDB Connected...'))
+        .catch((err) => {
+            console.error(err.message);
+            process.exit(1);
+        });
+};
+
+module.exports = connectDB;
