4656045d87f66bf34ae88701d48196dbd5de927e
vishal <68952732+vishalr-22@users.noreply.github.com>
2021-11-25 00:04:58 +0530

    created layout, final completion in 1-2 days

--- client/src/Pages/RoomDetail.js
+++ client/src/Pages/RoomDetail.js
@@ -1,11 +1,180 @@
 import React from 'react'
 import Header from '../Components/Header'
+import DatePicker from '../Components/DatePicker'
 
 function RoomDetail() {
     return (
         <div>
             <Header />
-            Room Detail Page
+            <div className="max-w-[85rem] mx-auto px-8 sm:px-6 lg:px-8 py-20">
+                <div className="font-sans">
+                    <div className="block mt-7 text-2xl font-semibold">
+                        Lonavala Villas - The Serenity Villa - 3BHK
+                    </div>
+                    <div className="block mt-0 text-lg text-gray-600 font-semibold">
+                    4.71 (120 reviews) · Lonavala, Maharashtra, India
+                    </div>
+                    <div>
+                        <div className="block mt-7 text-2xl text-gray-900 font-semibold">
+                            Entire villa hosted by Vishal
+                        </div>
+                        
+                        <div className="block mt-0 text-lg text-gray-600 ">
+                            12 guests · 3 bedrooms · 3 beds · 3 bathrooms
+                        </div>
+                    </div>
+                    <div className="divide-y divide-grey-400 md:divide-y-8">
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="grid grid-cols-2 gap-4">
+                        <div>
+                            <div className="flex flex-col justify-center mt-7" style={{width:"90%",height:"100px"}}>
+                                <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"200px"}}>
+                                    <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                        <h3 className="font-black text-gray-800 md:text-3xl text-xl">Entire home</h3>
+                                        <p className="md:text-lg text-gray-500 text-base">You’ll have the villa to yourself.</p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex flex-col justify-center mt-7" style={{width:"90%",height:"100px"}}>
+                                <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"200px"}}>
+                                    <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                        <h3 className="font-black text-gray-800 md:text-3xl text-xl">Entire home</h3>
+                                        <p className="md:text-lg text-gray-500 text-base">You’ll have the villa to yourself.</p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex flex-col justify-center mt-7" style={{width:"90%",height:"100px"}}>
+                                <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"200px"}}>
+                                    <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                        <h3 className="font-black text-gray-800 md:text-3xl text-xl">Entire home</h3>
+                                        <p className="md:text-lg text-gray-500 text-base">You’ll have the villa to yourself.</p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex flex-col justify-center mt-7" style={{width:"90%",height:"100px"}}>
+                                <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"200px"}}>
+                                    <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                        <h3 className="font-black text-gray-800 md:text-3xl text-xl">Entire home</h3>
+                                        <p className="md:text-lg text-gray-500 text-base">You’ll have the villa to yourself.</p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="mt-7 pl-5">
+                        <h3 className="font-black text-gray-800 md:text-3xl text-xl mt-7">Enhanced Clean</h3>
+                        <div class="grid grid-cols-2 gap-4">
+                            <div class="grid grid-cols-2 gap-4">
+                                <div className="flex flex-col justify-center p-2">
+                                    <p className="p-3 pl-5 md:text-lg text-gray-500 text-base">Kitchen</p>
+                                </div>
+                                <div className="flex flex-col justify-center p-2">
+                                    <p className="p-3 pl-5 md:text-lg text-gray-500 text-base">Kitchen</p>
+                                </div>
+                                <div className="flex flex-col justify-center p-2">
+                                    <p className="p-3 pl-5 md:text-lg text-gray-500 text-base">Kitchen</p>
+                                </div>
+                                <div className="flex flex-col justify-center p-2">
+                                    <p className="p-3 pl-5 md:text-lg text-gray-500 text-base">Kitchen</p>
+                                </div>
+                                <div className="flex flex-col justify-center p-2">
+                                    <p className="p-3 pl-5 md:text-lg text-gray-500 text-base">Kitchen</p>
+                                </div>
+                                <div className="flex flex-col justify-center p-2">
+                                    <p className="p-3 pl-5 md:text-lg text-gray-500 text-base">Kitchen</p>
+                                </div>
+                            
+                            </div>
+                            <div>
+                            <div className="flex flex-col justify-center mt-7" style={{width:"90%",height:"100px"}}>
+                                <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"200px"}}>
+                                    <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                        <p className="font-black text-gray-800 md:text-3xl text-xl">Rs 8900/night</p>
+                                        <p className="md:text-lg text-gray-500 text-base">You’ll have the villa to yourself.</p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>       
+                        <button class="mt-7 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                            See All Amenities
+                        </button>    
+                    
+                    </div>
+                    <div>
+                        <h3 className="font-black text-gray-800 md:text-3xl text-xl mt-7">Reviews</h3>
+                        <div class="grid grid-cols-2 gap-4">
+                            <div >
+                                <div className="flex flex-col justify-center mt-7">
+                                    <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"300px"}}>
+                                        <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                            <h3 className="font-black text-gray-800 md:text-3xl text-xl">Mugdha</h3>
+                                            November 2021
+                                            <p className="md:text-lg text-gray-500 text-base">We really enjoyed our stay in the serenity villa.. all of us loved the property, the outdoor games as well the carrom, indoors..
+    The Villa is nicely designed.. especially loved the tall windows in the living room.</p>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div >
+                                <div className="flex flex-col justify-center mt-7">
+                                    <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"300px"}}>
+                                        <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                            <h3 className="font-black text-gray-800 md:text-3xl text-xl">Mugdha</h3>
+                                            November 2021
+                                            <p className="md:text-lg text-gray-500 text-base">We really enjoyed our stay in the serenity villa.. all of us loved the property, the outdoor games as well the carrom, indoors..
+    The Villa is nicely designed.. especially loved the tall windows in the living room.</p>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div >
+                                <div className="flex flex-col justify-center mt-7">
+                                    <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"300px"}}>
+                                        <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                            <h3 className="font-black text-gray-800 md:text-3xl text-xl">Mugdha</h3>
+                                            November 2021
+                                            <p className="md:text-lg text-gray-500 text-base">We really enjoyed our stay in the serenity villa.. all of us loved the property, the outdoor games as well the carrom, indoors..
+    The Villa is nicely designed.. especially loved the tall windows in the living room.</p>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div >
+                                <div className="flex flex-col justify-center mt-7">
+                                    <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white" style={{width:"90%",height:"300px"}}>
+                                        <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                                            <h3 className="font-black text-gray-800 md:text-3xl text-xl">Mugdha</h3>
+                                            November 2021
+                                            <p className="md:text-lg text-gray-500 text-base">We really enjoyed our stay in the serenity villa.. all of us loved the property, the outdoor games as well the carrom, indoors..
+    The Villa is nicely designed.. especially loved the tall windows in the living room.</p>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
